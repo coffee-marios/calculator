11f07165a42628eb249fdc6fd88f310e66a9fdca
coffee-marios <mark_theo@yahoo.com>
2023-01-02 13:06:10 +0200

    Increase accuracy floating numbers.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ elementOperators.forEach(el=>{
 
 
 let digitsNumber = "";
-let hold = "0"; 
+let hold = 0; 
 let operation = "start";
 let add = 0;
 let subtract = 0;
@@ -22,12 +22,32 @@ let divide = 1;
 let appear = ""; 
 let outcome = 0;
 let registerOperator = false;
+let prosimo = 1;
+let decimal = 0;
 
 
 function clickNumber(e) {
     
     let gotNumber = e.target.dataset.number;
     registerOperator = false;
+    console.log(decimal)
+
+    // We don't want to have many decimal points
+    if (gotNumber === "."){
+        decimal += 1; 
+ 
+    } 
+    if (decimal >= 2 && gotNumber === ".") {
+            console.log(decimal)
+            return;
+        }
+
+    // We don't want to begin with many zeros: 0000000000000000034
+    if (digitsNumber === "0" && gotNumber !== "."){
+        return;
+    }
+
+    
 
      if (appear !== "" && operation === "start"){
         digitsNumber += gotNumber;
@@ -43,102 +63,90 @@ function clickNumber(e) {
 
         //hold = Number(e.target.dataset.number);
     } 
+
+    /*
+    After we use an operator:
+    hold is equal to the outcome or the first number we wrote  
+
+    */
    
     if (operation === "add"){
             console.log(33, digitsNumber, `add: ${add}, hold: ${hold} `);
 
         if (digitsNumber === ""){
             
-            add += Number(gotNumber);
-            outcome = Number(hold) + Number(add);
-            
+            add = Number(gotNumber) * prosimo;
             digitsNumber = gotNumber;
             appear = appear + digitsNumber;
-            console.log(33, digitsNumber, `add: ${add}, hold: ${hold} `);
             
 
         } else if (digitsNumber !== ""){
             digitsNumber += gotNumber;
             add = digitsNumber;
-            outcome = Number(hold) + Number(add);
-
-
             appear += gotNumber;
-            
-
-            console.log(33, digitsNumber, `add: ${add}, hold: ${hold} `);
             } 
-    } 
+            outcome = (Number(hold) * 10 + Number(add) * 10) /10;
+            console.log(33, digitsNumber, `add: ${add}, hold: ${hold} `);
+        } 
+
     if (operation === "subtract"){
         console.log(33, digitsNumber, `delete: ${subtract}, hold: ${hold} `);
 
         if (digitsNumber === ""){
             
-            subtract += Number(gotNumber);
-            outcome = Number(hold) - Number(subtract);
+            subtract = Number(gotNumber) * prosimo;
             
             digitsNumber = gotNumber;
             appear = appear + digitsNumber;
-            console.log(33, digitsNumber, `delete: ${subtract}, hold: ${hold} `);
+            
         } else if (digitsNumber !== ""){
             digitsNumber += gotNumber;
-            subtract = digitsNumber;
-            outcome = Number(hold) - Number(subtract);
-
-
+            subtract = digitsNumber * prosimo;
             appear += gotNumber;
-            
-
-            console.log(33, digitsNumber, `delete: ${subtract}, hold: ${hold} `);
             } 
-} 
-if (operation === "multiply"){
-    console.log(33, digitsNumber, `multiply: ${multiply}, hold: ${hold} `);
-
-    if (digitsNumber === ""){
-        
-        multiply *= Number(gotNumber);
-        outcome = Number(hold) * Number(multiply);
-        
-        digitsNumber = gotNumber;
-        appear = appear + digitsNumber;
-        console.log(33, digitsNumber, `multiply: ${multiply}, hold: ${hold} `);
-    } else if (digitsNumber !== ""){
-        digitsNumber += gotNumber;
-        multiply = digitsNumber;
-        outcome = Number(hold) * Number(multiply);
-
-
-        appear += gotNumber;
-        
-
-        console.log(33, digitsNumber, `multiply: ${multiply}, hold: ${hold} `);
+            outcome = (Number(hold) * 10 - Number(subtract) * 10) /10;
+            console.log(33, digitsNumber, `delete: ${subtract}, hold: ${hold} `);
         } 
-} 
+    
+        if (operation === "multiply"){
+            console.log(33, digitsNumber, `multiply: ${multiply}, hold: ${hold} `);
 
-if (operation === "divide"){
-    console.log(33, digitsNumber, `divide: ${divide}, hold: ${hold} `);
-
-    if (digitsNumber === ""){
+            if (digitsNumber === ""){
         
-        divide *= Number(gotNumber);
-        outcome = Number(hold) / Number(divide);
-        
-        digitsNumber = gotNumber;
-        appear = appear + digitsNumber;
-        console.log(33, digitsNumber, `divide: ${divide}, hold: ${hold} `);
-    } else if (digitsNumber !== ""){
-        digitsNumber += gotNumber;
-        multiply = digitsNumber;
-        outcome = Number(hold) / Number(divide);
+                multiply = multiply * Number(gotNumber) * prosimo;
+                //outcome = ((Number(hold)*10) * (Number(multiply)*10))/100;
+                
+                digitsNumber = gotNumber;
+                appear = appear + digitsNumber;
+                
+            } else if (digitsNumber !== ""){
+                digitsNumber += gotNumber;
+                multiply = Number(digitsNumber) * prosimo;
+                //outcome = Number(hold) * Number(multiply);
+                appear += gotNumber;
+            
+                }
+                outcome = ((Number(hold)*10) * (Number(multiply)*10))/100;
+                console.log(33, digitsNumber, `multiply: ${multiply}, hold: ${hold} `);
+            
+            } 
 
+            if (operation === "divide"){
 
-        appear += gotNumber;
-        
+                if (digitsNumber === ""){
+            
+                    divide = Number(gotNumber) * prosimo;
+                    digitsNumber = gotNumber;
+                    appear = appear + digitsNumber;
+                    
+                } else if (digitsNumber !== ""){
+                    digitsNumber += gotNumber;
+                    divide = Number(digitsNumber) * prosimo;
+                    appear += gotNumber;
+                    } 
+                    outcome = ((Number(hold) * 10) / (Number(divide) *10)) / 100;
 
-        console.log(33, digitsNumber, `divide: ${divide}, hold: ${hold} `);
-        } 
-}
+            }
 
 
     
@@ -147,14 +155,33 @@ if (operation === "divide"){
 }
 
 function clickOperator(e) {
+
+    // We are ready to pass to a new number
+    decimal = 0;
+
+    if (e.target.dataset.operator === "subtract" && registerOperator === true){
+        digitsNumber = "";
+        appear += " - ";
+        prosimo *= -1;
+        console.log(`${appear}`);
+        return;
+
+    }
+
+    prosimo = 1;
     
 
     if(e.target.dataset.operator === "clear"){
-        hold = "0";
-        add = "0";
+        hold = 0;
+        add = 0;
+        divide = 0;
+        subtract = 0;
+        multiply = 0;
+        outcome = 0;
         appear = "";
         operation = "start";
         digitsNumber = "";
+        prosimo = 1;
         console.clear();
     }
     if(e.target.dataset.operator === "add" && registerOperator === false){
@@ -163,8 +190,6 @@ function clickOperator(e) {
         appear += " + ";
         hold = outcome;
         add = 0;
-        //outcome += Number(hold); 
-
             
         console.log(`${appear}`);
     }
@@ -175,8 +200,6 @@ function clickOperator(e) {
         appear += " - ";
         hold = outcome;
         subtract = 0;
-        //outcome += Number(hold); 
-
             
         console.log(`${appear}`);
     } 
@@ -187,9 +210,7 @@ function clickOperator(e) {
         appear += " * ";
         hold = outcome;
         multiply = 1;
-        //outcome += Number(hold); 
 
-            
         console.log(`${appear}`);
     }
 
@@ -199,22 +220,22 @@ function clickOperator(e) {
         appear += " / ";
         hold = outcome;
         divide = 1;
-        //outcome += Number(hold); 
-
             
         console.log(`${appear}`);
     }
 
-
-
-
     if(e.target.dataset.operator === "equal"){
-        //outcome = Number(hold) + Number(outcome);
-        console.log(outcome, hold);
-        hold = "0";
-        appear = "";
+        appear = outcome;
+        console.log(appear);
+
+        hold = 0;
+        add = 0;
+        divide = 0;
+        subtract = 0;
+        multiply = 0;
         operation = "start";
         digitsNumber = "";
+        
     }
     
 
